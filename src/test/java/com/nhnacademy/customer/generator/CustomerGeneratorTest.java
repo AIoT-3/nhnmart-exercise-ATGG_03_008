50ed63d1b4de5a4c848c9a7db9d74bbd7cd2bd6b
{kim75503} <kim755030@naver.com>
2026-01-22 10:50:51 +0900

    step5

--- src/test/java/com/nhnacademy/customer/generator/CustomerGeneratorTest.java
+++ src/test/java/com/nhnacademy/customer/generator/CustomerGeneratorTest.java
@@ -28,38 +28,46 @@ class CustomerGeneratorTest {
 
     @BeforeEach
     void setUp() {
-        // TODO#4-6 enteringQueue 대기열을 capacity = 5로 초기화합니다.
-
-
-        // TODO#4-7 enteringQueue를 이용해서 customerGenerator 객체를 생성합니다.
-
+        // TODO#4-5 enteringQueue 대기열을 capacity = 5로 초기화합니다.
+        enteringQueue = new EnteringQueue(5);
+        // TODO#4-6 enteringQueue를 이용해서 CustomerGenerator 객체를 생성합니다.
+        customerGenerator = new CustomerGenerator(enteringQueue);
     }
 
     @Test
     @DisplayName("enteringQueue is null")
     void constructorTest(){
-        // TODO#4-8 enteringQueue == null 이면 IllegalArgumentException이 발생하는지 검증합니다.
+        // TODO#4-7 enteringQueue == null 이면 IllegalArgumentException 발생하는지 검증합니다.
 
+        assertThrows(IllegalArgumentException.class, () ->{
+            new CustomerGenerator(null);
+        });
     }
 
     @Test
-    @DisplayName("10초 동안 Customer 객체가 enteringQueue 대기열 등록")
+    @DisplayName("10초 동안 customer 객체가 enteringQueue 대기열 등록")
     void generatorTest() throws InterruptedException {
 
-        // TODO#4-9 customerGenerator를 이용해서 customerGeneratorThread를 초기화하고 실행합니다.
-
-
-        // TODO#4-10 10초 대기합니다.
-
-
-        // TODO#4-11 customerGeneratorThread를 종료합니다.
-
+        // TODO#4-8 customerGenerator를 이용해서 customerGeneratorThread 초기화하고, 실행합니다.
+        Thread customerGeneratorThread = new Thread(customerGenerator);
+        customerGeneratorThread.start();
+        // TODO#4-9 10초 대기합니다.
+        Thread.sleep(10000);
+        // TODO#4-10 customerGeneratorThread를 종료합니다.
+        customerGeneratorThread.interrupt();
+        customerGeneratorThread.join();
+        // assertions.assertAll 참고 ( ctrl + click or cmd + click)
+        // https://junit.org/junit5/docs/current/api/org.junit.jupiter.api/org/junit/jupiter/api/Assertions.html#assertAll(java.lang.String,org.junit.jupiter.api.function.Executable...)
 
         Assertions.assertAll(
-                // TODO#4-12 interrupt 발생 시 customerGeneratorThread의 상태가 TERMINATED 상태인지 검증
-                ()->Assertions.assertTrue(true),
-                // TODO#4-13 enteringQueue(대기열) 최대 Queue Size가 5 <-- 10초 동안 최대 5명의 고객이 대기열에 등록되었는지 검증합니다.
-                ()->Assertions.assertTrue(true)
+                ()->{
+                    // TODO#4-11 interrupt 발생 시 customerGeneratorThread의 상태가 TERMINATED 상태인지 검증합니다.
+                    assertEquals(Thread.State.TERMINATED, customerGeneratorThread.getState());
+                },
+                ()->{
+                    // TODO#4-12 enteringQueue(대기열) 최대 Queue Size가 5 <-- 10초 동안 최대 5명의 고객이 대기열에 등록되었는지 검증합니다.
+                    assertTrue(enteringQueue.getQueueSize() <= 5);
+                }
         );
     }
 
--- src/test/java/com/nhnacademy/nhnmart/entring/EnteringQueueTest.java
+++ src/test/java/com/nhnacademy/nhnmart/entring/EnteringQueueTest.java
@@ -18,53 +18,87 @@ import org.junit.jupiter.api.*;
 import org.junit.platform.commons.function.Try;
 import org.junit.platform.commons.util.ReflectionUtils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @Slf4j
 class EnteringQueueTest {
     EnteringQueue enteringQueue;
     @BeforeEach
-    void setUp() {
+    void setUp() throws InterruptedException {
         enteringQueue = new EnteringQueue();
 
-        /*
+        /* TODO#3-9
             Customer{id=1, name='NHN아카데미1', money=1000000}
             ~
             Customer{id=99, name='NHN아카데미99', money=1000000}
             1~99 고객을 생성 후 enteringQueue 대기열에 등록합니다.
          */
-
+        for(int i = 1; i< 100; i++){
+            String name ="NHN아카데미" + i;
+            enteringQueue.addCustomer(new Customer(i, name, 1000000));
+        }
     }
 
     @Test
     @DisplayName("default queue capacity = 100")
     void constructorTest_InitCapacity() throws Exception {
-        // TODO#3-9 EnteringQueue.class의 capacity 필드가 100인지 검증합니다. (ReflectionUtils 사용)
-
+        Try<Object> capacity = ReflectionUtils.tryToReadFieldValue(EnteringQueue.class,"capacity",enteringQueue);
+        assertEquals(100, (int)capacity.get() );
     }
 
     @Test
     void addCustomer() throws Exception {
         // TODO#3-10 id=100인 고객을 enteringQueue에 등록하고 검증합니다.
-
+        enteringQueue.addCustomer(new Customer(100L, "NHN아카데미100", 1_000_000));
+        assertEquals(100, enteringQueue.getQueueSize());
     }
 
     @Test
     @DisplayName("queue - poll test")
-    void getCustomer() {
+    void getCustomer() throws Exception{
         // TODO#3-11 enteringQueue에서 enteringQueue.getCustomer() 호출 시 반환되는 값을 검증합니다.
-
+        Customer customer = enteringQueue.getCustomer();
+        assertEquals(1L, customer.getId());
     }
 
     @Test
-    @DisplayName("blocking queue test : queue size : 100, 101번째 customer를 추가한다면, consumer에 의해서 소비될 때까지 대기합니다.")
+    @DisplayName("blocking queue test : queue size : 100, 101번째 Customer를 추가한다면, Consumer에 의해서 소비될 때까지 대기합니다.")
     void blockingTest() throws Exception {
-        // TODO#3-12 producer Thread를 구현합니다.
-        // id=100, id=101 고객을 추가합니다.
 
-        // TODO#3-13 2초 대기 후 enteringQueue.getCustomer()를 호출하여 소비할 수 있도록 consumer Thread를 구현합니다.
+        Thread producer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Customer customer100= new Customer(100L, "NHN아카데미100", 100_0000);
+                log.debug("2초 대기 후 101-customer 추가됨");
+                Customer customer101= new Customer(101L, "NHN아카데미101", 100_0000);
 
-        // TODO#3-14 producer 또는 consumer가 실행 중이라면 대기합니다. yield()를 이용해서 구현하세요.
+                try {
+                    enteringQueue.addCustomer(customer100);
+                    enteringQueue.addCustomer(customer101);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        producer.start();
 
+        // TODO#3-12 2초 대기 후 enteringQueue.getCustomer() 호출해서 소비할 수 있도록 consumer Thread를 구현합니다.
+        Thread consumer = new Thread(() -> {
+            try{
+                Thread.sleep(2000);
+                enteringQueue.getCustomer();
+            }catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+        });
+        consumer.start();
+        // TODO#3-13 producer 또는 consumer 실행 중이라면 대기합니다. yield()를 이용해서 구현하세요.
+        while (producer.isAlive() || consumer.isAlive()) {
+            Thread.yield();
+        } // TODO#3-14 producer 또는 consumer가 실행 중이라면 대기합니다. yield()를 이용해서 구현하세요.
+        int actual = enteringQueue.getQueueSize();
         // TODO#3-15 enteringQueue의 size가 100인지 검증합니다.
-
+        assertEquals(100,actual);
     }
 }
+
